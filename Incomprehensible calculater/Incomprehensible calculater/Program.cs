dd7ef484f096c531e618c0ac3d3981f2a5cf2b77
TheNoblish <DrNoble2.0@gmail.com>
2020-02-14 16:24:05 +0100

    calculator

---
+++ Incomprehensible calculater/Incomprehensible calculater/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incomprehensible_calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = 0; int c = 0; int b; bool h = false; bool o = false;while (!h){Console.WriteLine("Enter first number"); try{q = Convert.ToInt32(Console.ReadLine());h = true;Console.WriteLine("First number = " + q);}catch{Console.WriteLine("Not a number");}}while (!o){Console.WriteLine("Enter second number");try{c = Convert.ToInt32(Console.ReadLine());o = true;Console.WriteLine("Second number = " + c);}catch{Console.WriteLine("Not a number");}}if (q == c){Console.WriteLine("Tripling sum of " + q + " and " + c);b = q + c + q + c + q + c;Console.WriteLine("Result is " + b);}else{Console.WriteLine("Finding sum of " + q + " and " + c);b = q + c;Console.WriteLine("Result is " + b);}Console.WriteLine("Now go away");Console.ReadLine();
+        }
+    }
+}
